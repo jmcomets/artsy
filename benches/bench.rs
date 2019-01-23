13b5ba8567433650deda70ae2705136f2de3affd
Jean-Marie Comets <jean.marie.comets@gmail.com>
2019-01-23 13:05:13 +0100

    Add bench with 100 words

--- benches/bench.rs
+++ benches/bench.rs
@@ -8,37 +8,39 @@ use artsy::Trie;
 
 use std::collections::HashMap;
 
-const ASCII_CHARS: &'static [u8] = b"abcdefghijklmnopqrstuvwxyz";
-
-struct BytesGenerator {
-    current: Vec<u8>,
-    next_ascii_char: usize,
+struct LexicographicBytesGenerator<'a> {
+    source_bytes: &'a [u8],
+    current_bytes: Vec<u8>,
+    next_byte_index: usize,
 }
 
-impl BytesGenerator {
-    fn new() -> Self {
-        BytesGenerator {
-            current: vec![],
-            next_ascii_char: ASCII_CHARS.len(),
+impl<'a> LexicographicBytesGenerator<'a> {
+    fn new(source_bytes: &'a [u8]) -> Self {
+        LexicographicBytesGenerator {
+            current_bytes: vec![],
+            next_byte_index: source_bytes.len(),
+            source_bytes: source_bytes,
         }
     }
 
     fn next_bytes(&mut self) -> &[u8] {
-        if self.next_ascii_char == ASCII_CHARS.len() {
-            self.next_ascii_char = 0;
-            self.current.push(0);
+        if self.next_byte_index == self.source_bytes.len() {
+            self.next_byte_index = 0;
+            self.current_bytes.push(0);
         }
 
-        let index = self.current.len() - 1;
-        self.current[index] = ASCII_CHARS[self.next_ascii_char];
-        self.next_ascii_char += 1;
+        let index = self.current_bytes.len() - 1;
+        self.current_bytes[index] = self.source_bytes[self.next_byte_index];
+        self.next_byte_index += 1;
 
-        &self.current
+        &self.current_bytes
     }
 }
 
-fn insertion_data(n: usize) -> Vec<(Vec<u8>, usize)> {
-    let mut gen = BytesGenerator::new();
+const ASCII_CHARS: &'static [u8] = b"abcdefghijklmnopqrstuvwxyz";
+
+fn ordered_ascii_words(n: usize) -> Vec<(Vec<u8>, usize)> {
+    let mut gen = LexicographicBytesGenerator::new(ASCII_CHARS);
     (0..n)
         .map(|_| gen.next_bytes().to_owned())
         .enumerate()
@@ -47,8 +49,8 @@ fn insertion_data(n: usize) -> Vec<(Vec<u8>, usize)> {
 }
 
 #[bench]
-fn trie_insertion(bench: &mut Bencher) {
-    let entries = insertion_data(1000);
+fn trie_100_ascii_words_insertion(bench: &mut Bencher) {
+    let entries = ordered_ascii_words(100);
 
     bench.iter(|| {
         let mut trie = Trie::for_ascii();
@@ -59,8 +61,8 @@ fn trie_insertion(bench: &mut Bencher) {
 }
 
 #[bench]
-fn hashmap_insertion(bench: &mut Bencher) {
-    let entries = insertion_data(1000);
+fn hashmap_100_ascii_words_insertion(bench: &mut Bencher) {
+    let entries = ordered_ascii_words(100);
 
     bench.iter(|| {
         let mut trie = HashMap::new();
@@ -71,8 +73,8 @@ fn hashmap_insertion(bench: &mut Bencher) {
 }
 
 #[bench]
-fn trie_retrieval(bench: &mut Bencher) {
-    let entries = insertion_data(1000);
+fn trie_100_ascii_words_retrieval(bench: &mut Bencher) {
+    let entries = ordered_ascii_words(100);
 
     let mut trie = Trie::for_ascii();
     for (key, value) in entries.iter() {
@@ -87,8 +89,66 @@ fn trie_retrieval(bench: &mut Bencher) {
 }
 
 #[bench]
-fn hashmap_retrieval(bench: &mut Bencher) {
-    let entries = insertion_data(1000);
+fn hashmap_100_ascii_words_retrieval(bench: &mut Bencher) {
+    let entries = ordered_ascii_words(100);
+
+    let mut map = HashMap::new();
+    for (key, value) in entries.iter() {
+        map.insert(key, value);
+    }
+
+    bench.iter(|| {
+        for (key, value) in entries.iter() {
+            assert_eq!(map.get(key), Some(&value));
+        }
+    })
+}
+
+// 1000
+
+#[bench]
+fn trie_1000_ascii_words_insertion(bench: &mut Bencher) {
+    let entries = ordered_ascii_words(1000);
+
+    bench.iter(|| {
+        let mut trie = Trie::for_ascii();
+        for (key, value) in entries.iter() {
+            trie.insert(key, value).unwrap();
+        }
+    })
+}
+
+#[bench]
+fn hashmap_1000_ascii_words_insertion(bench: &mut Bencher) {
+    let entries = ordered_ascii_words(1000);
+
+    bench.iter(|| {
+        let mut trie = HashMap::new();
+        for (key, value) in entries.iter() {
+            trie.insert(key, value);
+        }
+    })
+}
+
+#[bench]
+fn trie_1000_ascii_words_retrieval(bench: &mut Bencher) {
+    let entries = ordered_ascii_words(1000);
+
+    let mut trie = Trie::for_ascii();
+    for (key, value) in entries.iter() {
+        trie.insert(key, value).unwrap();
+    }
+
+    bench.iter(|| {
+        for (key, value) in entries.iter() {
+            assert_eq!(trie.get(key).unwrap(), Some(&value));
+        }
+    })
+}
+
+#[bench]
+fn hashmap_1000_ascii_words_retrieval(bench: &mut Bencher) {
+    let entries = ordered_ascii_words(1000);
 
     let mut map = HashMap::new();
     for (key, value) in entries.iter() {
